f5d2543ac882dfc8999d039860a3da99ca37d42a
candyolivia <candyoliviamawalim@gmail.com>
2016-03-27 22:57:42 +0700

    parser and coordinate converter

--- helloTriangle/main.cpp
+++ helloTriangle/main.cpp
@@ -4,15 +4,17 @@
 #include <stdlib.h>   // standard library (set of standard C functions
 #include <math.h>     // Math library (Higher math functions )
 #include "DrawHandler.h"
+#include "Coordinate.h"
+#include "Point.h"
 
 int window_1, window_2;
+int window_size = 500;
 
 // Main program
 int main(int argc, char** argv)
 {
-   glutInit(&argc, argv);
-   //glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize (500, 500);
+    glutInit(&argc, argv);
+   glutInitWindowSize (1000, 1000);
    glutInitWindowPosition (10, 10);
    window_1 = glutCreateWindow (argv[0]);
    glutSetWindowTitle("Three Triangles");
@@ -21,8 +23,8 @@ int main(int argc, char** argv)
    glutReshapeFunc(reshape_1);
    display_1();
 
-   glutInitWindowSize (500, 500);
-   glutInitWindowPosition (550, 10);
+   glutInitWindowSize (window_size, window_size);
+   glutInitWindowPosition (window_size + 50, 10);
    window_2 = glutCreateWindow (argv[0]);
    glutSetWindowTitle("Gradient Triangle");
    init ();
